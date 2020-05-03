37398765812f0f9774cc150c56982c1327a9f0d1
spenceruns <spenceruns@gmail.com>
2020-05-03 10:13:30 -0700

    Starting files

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
