950a6dfe2c59521813416fe34a7be8b5d0274e38
spenceruns <spenceruns@gmail.com>
2020-05-03 16:07:20 -0700

    Started base schemas and set up helpers to build data from schema

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import client from '../helpers/sanity'
+import builder from '../helpers/image-url'
+import imageObj from '../helpers/imageBuilder'
 import './app.scss';
 
-function App() {
+export default function App() {
+  const [test, setTest] = useState([]);
+  useEffect(() => {
+    const query = `*[_type == "images"]`;
+
+    client.fetch(query).then(data => {
+      setTest(data[0]);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App__header">
+        {test.length !== 0 &&
+          <img
+            src={builder(imageObj(test.image[0])).url()}
+            alt={test.name}
+            className="App__image" />}
         <h1>Happy Birthday Lucy!</h1>
       </header>
     </div>
   );
 }
-
-export default App;
---
+++ src/helpers/imageBuilder.js
@@ -0,0 +1,10 @@
+export default function buildImageObj(source) {
+  const imageObj = {
+    asset: { _ref: source.asset._ref || source.asset._id }
+  };
+
+  if (source.crop) imageObj.crop = source.crop;
+  if (source.hotspot) imageObj.hotspot = source.hotspot;
+
+  return imageObj;
+}
